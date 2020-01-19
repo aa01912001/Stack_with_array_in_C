39b8c1dcbde244f78e11d55521301545ed690d17
Ian <aa01912001@gmail.com>
2020-01-19 15:21:46 +0800

    create C project

---
+++ Stack_with_array/main.c
@@ -0,0 +1,14 @@
+/*
+Date: 2020/01/19
+Author: Ian
+Object: To implement a stack with integral array in C.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
